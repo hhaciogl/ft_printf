3bc4537c1df2ba3cde3ade7e71004e9aad1aedeb
Hasan Hacıoğlu <hhaciogl@student.42istanbul.com.tr>
2024-11-28 19:29:55 +0300

    add_hex_handlers

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:40:25 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/11/28 18:46:33 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/11/28 19:27:47 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,34 @@ int handle_u(unsigned n, int *is_err)
 		return (handle_d_and_i(n % 10000, is_err));
 }
 
+int handle_x(unsigned n, int x)
+{
+	char *hex_str;
+	char up_case;
+	
+	hex_str = "0123456789abcdef";
+	if (16 > n)
+	{
+		if (x)
+			return (write(1, &hex_str[n], 1));
+		else
+		{
+			up_case = ft_toupper(hex_str[n]);
+			return (write(1, &up_case, 1));
+		}		
+	}
+	else
+	{
+		if (x)
+			return (handle_x(n / 16, x) + write(1, &hex_str[n % 16], 1));
+		else
+		{
+			up_case = ft_toupper(hex_str[n % 16]);
+			return (handle_x(n / 16, x) + write(1, &up_case, 1));
+		}
+			
+	}
+}
 
 
 static int handle_formats(char *a, va_list lst_ptr, int *is_err)
@@ -75,6 +103,10 @@ static int handle_formats(char *a, va_list lst_ptr, int *is_err)
 		return (write(1, a, 1));
 	else if ('u' == *a)
 		return (handle_u(va_arg(lst_ptr, unsigned), is_err));
+	else if ('x' == *a)
+		return (handle_x(va_arg(lst_ptr, unsigned), 1));
+	else if ('X' == *a)
+		return (handle_x(va_arg(lst_ptr, unsigned), 0));
 	else
 	{
 		*is_err = -1;
