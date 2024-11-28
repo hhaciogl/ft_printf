b030d162bdfaf6ffa5d5d209f1487c98c60248bf
Hasan Hacıoğlu <hhaciogl@student.42istanbul.com.tr>
2024-11-28 18:33:17 +0300

    add_new_feats

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:40:25 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/11/28 17:49:19 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/11/28 18:29:15 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
 
 
 int handle_s(char *str)
@@ -30,22 +32,24 @@ int handle_c(int c)
 	return (write(1, &c, 1));
 }
 
-int handle_d(int n, int *is_err)
+int handle_d_and_i(int n, int *is_err)
 {
 	char	*str;
 	int		count;
 
-	str = ft_calloc(1, sizeof(int));
+	str = ft_calloc(1, 20);
 	if (NULL == str)
 	{
 		*is_err = -1;
 		return (-1);
 	}
-	ft_strlcpy(str, ft_itoa(n), sizeof(int));
+	ft_strlcpy(str, ft_itoa(n), 20);
 	count = handle_s(str);
 	free(str);
 	return (count);
 }
+
+
 
 static int handle_formats(char *a, va_list lst_ptr, int *is_err)
 {
@@ -54,6 +58,12 @@ static int handle_formats(char *a, va_list lst_ptr, int *is_err)
 		return (handle_s(va_arg(lst_ptr, char *)));
 	else if ('c' == *a)
 		return (handle_c(va_arg(lst_ptr, int)));
+	else if ('d' == *a)
+		return (handle_d_and_i(va_arg(lst_ptr, int), is_err));
+	else if ('i' == *a)
+		return (handle_d_and_i(va_arg(lst_ptr, int), is_err));
+	else if ('%' == *a)
+		return (write(1, a, 1));
 	else
 	{
 		*is_err = -1;
@@ -92,5 +102,9 @@ int	ft_printf(const char *format, ...)
 	va_start(lst_ptr, format);
 	out = _printf(format, lst_ptr, &is_err);
 	va_end(lst_ptr);
+	if (0 > is_err)
+	{
+		return (-1);
+	}
 	return (out);
 }
