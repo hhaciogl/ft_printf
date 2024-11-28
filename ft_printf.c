2bd3151e36599797e7de6df17c10c80b1c4bfe67
Hasan Hacıoğlu <hhaciogl@student.42istanbul.com.tr>
2024-11-28 17:00:37 +0300

    add_handlers

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 17:40:25 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/11/27 18:50:50 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/11/28 16:59:10 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,90 @@
 #include "./libft/libft.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int handle_str(char *str)
+
+int handle_s(char *str)
 {
-	if (str == NULL)
+
+	if (NULL == str)
 		return (write(1, "(null)", 6));
 	return (write(1, str, ft_strlen(str)));
 }
 
-
-int _printf(char *fmt, va_list va_args)
+int handle_c(int c)
 {
-	char	*vars;
-	char	*rest_of_str;
+	return (write(1, &c, 1));
+}
 
-	vars = "%s";
-	//abc
-	rest_of_str = ft_strnstr(fmt, vars, ft_strlen(fmt));
-	if(!rest_of_str)
-	{	//This is a string: %s
+int handle_d(int n, int *is_err)
+{
+	char *str;
+	int		count;
+
+	str = ft_calloc(1, sizeof(int));
+	if (NULL == str)
+	{
+		*is_err = -1;
+		return (-1);
+	}
+	ft_strlcpy(str, ft_itoa(n), sizeof(int));
+	count = handle_s(str);
+	free(str);
+	return (count);
+}
+
+static int handle_formats(char *a, va_list lst_ptr, int *is_err)
+{
+
+	if ('s' == *a)
+	{
+		return (handle_s(va_arg(lst_ptr, char *)));
+	}
+	if ('c' == *a)
+		return (handle_c(va_arg(lst_ptr, int)));
+	else
+	{
+		*is_err = 1;
+		return (-1);
+	}
+}
+
+
+static int _printf(const char *fmt, va_list lst_ptr, int *is_err)
+{
+	char *chr;
+	
+	is_err = 0;
+	if (fmt == NULL || 0 > is_err )
+		return (-1);
+	if (ft_strncmp(fmt, "", 1) == 0)
+	{		
+		return (0);
+	}
+	chr = ft_strchr(fmt, '%');
+	if (NULL == chr)
+	{
 		return (write(1, fmt, ft_strlen(fmt)));
 	}
-	if (ft_strncmp(rest_of_str, "%s") == 0)
-		return ;
+	else if (NULL == chr+1)
+	{
+		return (-1);
+	}
 	else
-		
-	// puts("==============");
-	// printf("%li", rest_of_str - fmt);
-	// puts("");
-	// puts("==============");
-	return (write(1, fmt, (rest_of_str -fmt)) + handle_str(va_arg(va_args, char *)));
+		return (write(1, fmt, chr-fmt) + handle_formats(chr + 1, lst_ptr, is_err) + _printf(chr + 2, lst_ptr, is_err));
 }
 
 
 int	ft_printf(const char *format, ...)
 {
-	char *fmt;
 	int	out;
-	va_list va_args;
+	va_list lst_ptr;
+	int is_err;
 
-	va_start(va_args, format);
-	fmt = ft_strdup(format);
-	if(fmt == NULL)
-	{
-		out = -1;
-	}
-	else
-	{
-		out = _printf(fmt, va_args);
-	}
-	va_end(va_args);
+	is_err = 0;
+	va_start(lst_ptr, format);
+	out = _printf(format, lst_ptr, &is_err);
+	va_end(lst_ptr);
 	return (out);
 }
